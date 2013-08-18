b3c5362ed01d13f86cbe4db9460c79341a9331d8
Jay De Leon <jdeleon@jdeleon.com>
2013-08-18 16:47:11 +1000

    added user model with devise

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130815130106) do
+ActiveRecord::Schema.define(:version => 20130818064233) do
 
   create_table "models", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(:version => 20130815130106) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "name"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
