6a2aadc6e75d73a58582f22c060c8ce563acc4f0
Jay De Leon <jdeleon@jdeleon.com>
2013-08-19 19:15:29 +1000

    image via url

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,7 +1,17 @@
 class Pin < ActiveRecord::Base
-  attr_accessible :description, :image
-  has_attached_file :image, styles: { medium: "240x240" }
+  attr_accessible :description, :image, :image_remote_url
+
   validates :description, presence: true
-  belongs_to :user
   validates :user_id, presence: true
+  has_attached_file :image, styles: { medium: "320x240>"}
+  validates_attachment :image, presence: true,
+                            content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] },
+                            size: { less_than: 5.megabytes }
+  belongs_to :user
+
+  def image_remote_url=(url_value)
+  	self.image = URI.parse(url_value) unless url_value.blank?
+  	super
+  end
+
 end
