3586b62d9e7d8f821272ce91bf33d8010861aab0
Jay De Leon <jdeleon@jdeleon.com>
2013-08-19 12:25:49 +1000

    associate users

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,6 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me, :name
   # attr_accessible :title, :body
   validates_presence_of :name
+
+  has_many :pins
 end
